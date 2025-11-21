8cdeead4f2c7417a29166567797a1b5e591300d5
gustavomaisatto <gustavomaisattok@gmail.com>
2025-11-21 17:27:38 -0300

    initial commit

---
+++ src/main/java/com/facilita/api/FacilitaAppApplication.java
@@ -0,0 +1,13 @@
+package com.facilita.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilitaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilitaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facilita/api/FacilitaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facilita.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilitaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
